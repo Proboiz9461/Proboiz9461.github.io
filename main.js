26874c1a76ecf8574d2684813fde89c754f0e144
Proboiz9461 <chauhanshaurya120@gmail.com>
2025-12-21 19:38:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-const hoverSound = new Audio("sounds/hover.mp3");
-const clickSound = new Audio("sounds/click.mp3");
-
-document.querySelectorAll(".launcher-tile, a").forEach(el => {
-  el.addEventListener("mouseenter", () => {
-    hoverSound.currentTime = 0;
-    hoverSound.play();
-  });
-
-  el.addEventListener("click", () => {
-    clickSound.currentTime = 0;
-    clickSound.play();
-  });
-});
-
 function openGame(gameFile) {
-  setTimeout(() => {
-    window.location.href = `play.html?game=${gameFile}`;
-  }, 200);
+  window.location.href = `play.html?game=${gameFile}`;
 }
