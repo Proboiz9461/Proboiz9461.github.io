cbc7864ea3203508b9e26cb960a4ed193ea47af0
Proboiz9461 <chauhanshaurya120@gmail.com>
2025-12-21 18:52:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", e => {
-    document.body.style.opacity = "0";
+const hoverSound = new Audio("sounds/hover.mp3");
+const clickSound = new Audio("sounds/click.mp3");
+
+document.querySelectorAll(".launcher-tile, a").forEach(el => {
+  el.addEventListener("mouseenter", () => {
+    hoverSound.currentTime = 0;
+    hoverSound.play();
+  });
+
+  el.addEventListener("click", () => {
+    clickSound.currentTime = 0;
+    clickSound.play();
   });
 });
+
+function openGame(gameFile) {
+  setTimeout(() => {
+    window.location.href = `play.html?game=${gameFile}`;
+  }, 200);
+}
