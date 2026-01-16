4f9a61e49d1658963b70966acf0c88507e98dcc8
Proboiz9461 <chauhanshaurya120@gmail.com>
2026-01-16 18:16:51 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAnd61h85OmJiC19GqpT4lpvSBpDExDzpM",
+  authDomain: "chat-5298d.firebaseapp.com",
+  projectId: "chat-5298d",
+  storageBucket: "chat-5298d.firebasestorage.app",
+  messagingSenderId: "24015709772",
+  appId: "1:24015709772:web:3442ad774b8b59138676e9",
+  measurementId: "G-Q1DCB9X2Q3"
 };
 
 firebase.initializeApp(firebaseConfig);
