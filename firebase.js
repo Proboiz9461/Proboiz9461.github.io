2433c2278044275d8e5653f46dfb59c5da5f3191
Proboiz9461 <chauhanshaurya120@gmail.com>
2026-02-07 20:04:22 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_Na0eivLj652OIfV94hPhBNUC0k8CndI",
+  authDomain: "breathers-2026.firebaseapp.com",
+  projectId: "breathers-2026",
+  appId: "1:73555421893:web:987d540dd10f47da71deac"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
